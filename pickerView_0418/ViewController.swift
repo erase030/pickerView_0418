6289aa3f3a723d939fe8422cd057ed9a3ae2b462
dit08 <dit@imac-3.local>
2019-04-18 12:00:46 +0900

    pickerView

--- pickerView_0418/ViewController.swift
+++ pickerView_0418/ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return color.count
+    }
+    
+    var color = ["red", "green", "blue"]
+    var emotion = ["Hot", "calm", "beach", "sky","elepants"]
+    
 
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var lbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        picker.dataSource = self
+        picker.delegate = self
+        lbl.text = color[0]
+    }
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return color[row]
+    }
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lbl.text = color[row]
+        print("component=\(component)")
+        print("row=\(row)")
     }
 
 
