3e92ebf29dc3a89d07a5c99c7c03490042b257b9
dit08 <dit@imac-3.local>
2019-04-18 11:41:40 +0900

    Initial Commit

---
+++ pickerView_0418/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pickerView_0418
+//
+//  Created by dit08 on 2019. 4. 18..
+//  Copyright © 2019년 dit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
